51cbfc08cc7f6b7fcac2944d921b0133885f0e03
akmystery <arkar.edward19@gmail.com>
2018-10-17 12:06:44 +0800

    Remove Code

--- Lab 3/BackEnd Code/app/controllers/sms.controller.js
+++
@@ -1,7 +0,0 @@
-const SMSAPIController = require('../controllers/smsAPI.controller.js');
-
-exports.processSMSResponse = SMSAPIController.processSMSResponse;
-
-exports.sendSMSControler = (messageBody) => {
-    SMSAPIController.sendSMS(messageBody);
-}
